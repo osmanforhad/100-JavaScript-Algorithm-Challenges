eedf077e43c272adb601ea322fc83ee67ff0b98f
osmanforhad <osmanforhad93@gmail.com>
2025-06-18 12:40:00 +0600

    question setup

---
+++ Challange-2.js
@@ -0,0 +1,6 @@
+//write a function that returns the some of two numbers that all numbers all numbers reguadless of param
+/**
+ * for param1= 1 and param2= 2, the output should be add(param1 + param2) = 3
+ *
+ * and there must have to Arithmatic operators, rest operator and foreach()
+ */
