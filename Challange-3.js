560149a24a28629087c67ace4430688f975f4e55
osmanforhad <osmanforhad93@gmail.com>
2025-06-18 17:33:07 +0600

    solve the channlange three

--- Challange-3.js
+++ Challange-3.js
@@ -1,2 +1,13 @@
 //Give a rectangular matrix of characters, add a border of asterisks(*) to it
 //there have to be concat() unshift() push()
+function addBorder(pictures) {
+  let wall = "*".repeat(pictures[0].length + 2);
+  pictures.unshift(wall);
+  pictures.push(wall);
+  for (let i = 1; i < pictures.length - 1; i++) {
+    pictures[i] = "*".concat(pictures[i], "*");
+  }
+  return pictures;
+}
+const result = addBorder(["abc", "ded"]);
+console.log(result);
