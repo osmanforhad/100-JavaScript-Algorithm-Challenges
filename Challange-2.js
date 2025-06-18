975ccde0f8eea5027ebfdef2d4ed61ac08d743c4
osmanforhad <osmanforhad93@gmail.com>
2025-06-18 12:54:49 +0600

    solved js algorithm challange 2

--- Challange-2.js
+++ Challange-2.js
@@ -2,5 +2,24 @@
 /**
  * for param1= 1 and param2= 2, the output should be add(param1 + param2) = 3
  *
- * and there must have to Arithmatic operators, rest operator and foreach()
+ * and there must have to Arithmatic operators, rest operator and forEach()
  */
+
+//Method one and simple solution
+function sum(param1, param2) {
+  return param1 + param2;
+}
+const result = sum(1, 2);
+console.log(result);
+
+//Method two and expected as per Question
+function totalSum(...param1) {
+  let total = 0;
+  param1.forEach((perElement) => {
+    total += perElement;
+  });
+  return total;
+}
+
+const finalResutl = totalSum(1, 2);
+console.log(finalResutl);
